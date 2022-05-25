709a4ab91fb46f33401ec5613c0fbcfc146a73cf
Strasser Martin <StrasserM180084@sus.htl-grieskirchen.at>
2022-05-25 12:31:02 +0200

    Added Helper Methods

---
+++ Project_3q4t10n5/Project_3q4t10n5/Helpers.cs
@@ -0,0 +1,28 @@
+﻿namespace Project_3q4t10n5;
+
+public class Helpers
+{
+    public double[][] ReadCSV(string path)
+    {
+        var lines = File.ReadAllLines(path);
+        double[][] result = new double[lines.Length][];
+
+        for(int i = 0; i < lines.Length; i++)
+        {
+            var cols = lines[i].Split(';');
+            result[i] = new double[cols.Length];
+
+            for(var j = 0; j < cols.Length; j++)
+            {
+                result[i][j] = Convert.ToDouble(cols[j]);
+            }
+        }
+
+        return result;
+    }
+
+    public void PrintMatrix(double[][] matrix)
+    {
+        Console.WriteLine(matrix);
+    }
+}
---
+++ Project_3q4t10n5/Project_3q4t10n5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Project_3q4t10n5;
+
+public class Program
+{
+    private static string _file_path = "Files/";
+    private static Helpers _helpers =  new Helpers();
+
+    public static void Main(string[] args)
+    {
+        var currentExample = "Example.csv";
+
+        Console.WriteLine("Starting Project_3q4t10n5 \n");
+        Console.WriteLine($"Using Example Files/{currentExample}");
+
+        var matrix = _helpers.ReadCSV($"Files/{currentExample}");
+        _helpers.PrintMatrix(matrix);
+
+
+        Console.ReadKey();
+    }
+}
