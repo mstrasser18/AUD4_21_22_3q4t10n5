6a8b26958d64aab4745c3375a9f9e838c02238f9
Strasser Martin <StrasserM180084@sus.htl-grieskirchen.at>
2022-06-08 12:36:11 +0200

    Added Template Generator

--- Project_3q4t10n5/Project_3q4t10n5/MatrixCalculator.cs
+++ Project_3q4t10n5/Project_3q4t10n5/MatrixCalculator.cs
@@ -10,9 +10,31 @@ public class MatrixCalculator
 {
     public void SolveMatrixMatrixMultiplication(double[][] c, out double[][] a, out double[][] b)
     {
+        CreateTemplateMatrices(c.Length, out a, out b);
 
+        //Hartcoded for 3x3 Matrices
 
-        a = c;
-        b = c;
+    }
+
+    private void CreateTemplateMatrices(int size, out double[][] a, out double[][] b)
+    {
+        a = new double[size][];
+
+        for(int i = 0; i < a.Length; i++)
+        {
+            a[i] = new double[size];
+            for(int j = 0; j < a[i].Length; j++)
+            {
+                //Diagonals constant value 1
+                if(i == j) a[i][j] = 1;
+            }
+        }
+
+        b = new double[size][];
+
+        for(int i = 0; i < b.Length; i++)
+        {
+            b[i] = new double[size];
+        } 
     }
 }
